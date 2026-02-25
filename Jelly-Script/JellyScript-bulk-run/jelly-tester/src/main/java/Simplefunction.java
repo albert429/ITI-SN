2757b189f778045f4c049e787b223230ffb4f6ff
albert429 <albertalfred429@gmail.com>
2026-02-25 12:47:21 +0200

    Added All Jelly Script Files

---
+++ Jelly-Script/JellyScript-bulk-run/jelly-tester/src/main/java/Simplefunction.java
@@ -0,0 +1,9 @@
+public class Simplefunction {
+    public String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
